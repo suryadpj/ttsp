ab0354f6b330aab77e77d6859859fd32d23446e5
suryadpj <surya.dewantara@apsupports.com>
2023-05-22 17:55:52 +0700

    pembatasan akses

--- tcm/routes/web.php
+++ tcm/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->middleware('auth');;
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
